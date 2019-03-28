27a0fcaa4ce6f1e5e105f51de8dcf1726f54a8d5
github username <github email address>
2019-03-28 19:13:04 -0400

    Try to use twitter omniauth, but needs approval to use the API from Twitter. May not be worth it :construction:

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,7 +3,6 @@ class CallbacksController < ApplicationController
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
   end
-  
 
   def failure
     flash[:error] = 'There was a problem signing you in. Please register or try signing in later.' 
