158a404fbd89f5cd8fe6cf163ce431aff1f2ed1c
github username <github email address>
2019-03-28 09:56:26 -0400

    Delete username field on login page and Add logged_in_as helper method

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,21 +1,11 @@
 class CallbacksController < ApplicationController
   def google_oauth2
     @user = User.from_omniauth(request.env['omniauth.auth'])
+    # binding.pry
+    #username is returning nil when logging in with google. How can I grub the username?
     sign_in_and_redirect @user
   end
-  # def google_oauth2
-  #   # This one throws an error because find_for_google_oauth2 is not defined.
-  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
-  #   @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-
-  #   if @user.persisted?
-  #     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-  #     sign_in_and_redirect @user, :event => :authentication
-  #   else
-  #     session["devise.google_data"] = request.env["omniauth.auth"]
-  #     redirect_to new_user_registration_url
-  #   end
-  # end
+  
 
   def failure
     flash[:error] = 'There was a problem signing you in. Please register or try signing in later.' 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def logged_in_as
+    if current_user.username.present?
+      "Logged in as #{current_user.username}" 
+    else   
+      "Logged in as #{current_user.email}"
+    end
+  end
+
 end
