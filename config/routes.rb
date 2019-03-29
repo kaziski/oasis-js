35e8686b1487f360c3f199d1f8eeb774ce652302
github username <github email address>
2019-03-29 00:09:15 -0400

    Add authenticated root route:dizzy:

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  resources :actions
-  resources :plants
-  root 'welcome#index'
-  # unauthenticated do
-  #   root :to => 'welcome#index'
-  # end
+  # resources :actions
+  # resources :plants
+  # root 'welcome#index'
+  unauthenticated do
+    root :to => 'welcome#index'
+  end
  
-  # authenticated do
-  #   root :to => 'plant#index'
-  # end
+  authenticated do
+    root :to => 'plants#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 end
