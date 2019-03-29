4ba626941341c2e6edfc3c974cf06368329e4b86
github username <github email address>
2019-03-29 01:02:13 -0400

    Add spec.md:memo:

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # resources :actions
-  # resources :plants
-  # root 'welcome#index'
+  
   unauthenticated do
     root :to => 'welcome#index'
   end
@@ -9,6 +7,9 @@ Rails.application.routes.draw do
   authenticated do
     root :to => 'plants#index'
   end
+
+  resources :actions
+  resources :plants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 end
