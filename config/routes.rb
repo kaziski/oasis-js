56ca81888bbbe38a6af886fa97999a50e4ccfd76
github username <github email address>
2019-03-28 23:43:53 -0400

    Add Plant resource. Change welcome message:wave:

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  resources :plants
   root 'welcome#index'
+  # unauthenticated do
+  #   root :to => 'welcome#index'
+  # end
+ 
+  # authenticated do
+  #   root :to => 'plant#index'
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 end
