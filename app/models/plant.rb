8ec62df705489a75881533023aaad1f9654b521f
github username <github email address>
2019-04-03 01:48:36 -0400

    Bug fix with actions_attributes

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,9 @@
 class Plant < ApplicationRecord
   belongs_to :user
-  has_many :plantactions
-  has_many :actions, through: :plantsactions
+  has_many :plants_action
+  has_many :actions, through: :plants_action
+  accepts_nested_attributes_for :actions, :allow_destroy => true
+
   validates :name, uniqueness: true, presence: true
   validates_inclusion_of :in_the_garden?, in: [true, false]
   validates_inclusion_of :edible?, in: [true, false]
@@ -18,4 +20,14 @@ class Plant < ApplicationRecord
   def self.show_all_annual(user)
     where(user_id: user.id, annual: true )
   end
+
+  def actions_attributes=(action_attributes)
+    action_attributes.values.each do |action_attribute|
+      if action_attribute[:action_name].present?
+          action = Action.find_or_create_by(action_attribute)
+          self.actions << action
+      end
+    end
+  end
+ 
 end
