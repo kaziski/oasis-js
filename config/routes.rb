5421947da9f06e4642bce476a80256487143df82
github username <github email address>
2019-04-03 01:58:01 -0400

    Delete code commented out

--- config/routes.rb
+++ config/routes.rb
@@ -18,16 +18,6 @@ Rails.application.routes.draw do
   get '/plants/annual', to: 'plants#annual'
 
   resources :plants
-  # get '/plants', to: 'plants#index'
-  # get '/plants/new'
-  # post '/plants', to: 'plants#create'
-  # get '/plants/in_garden', to: 'plants#in_garden'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 end
-
-  # post '/toys', to: 'toys#create'
-  # get '/toys/:id', to: 'toys#show', as: 'toy'
-  # get '/toys/:id/edit', to: 'toys#edit', as: 'toy_edit'
-  # patch '/toys/:id', to: 'toys#update'
-  # delete '/toys/:id', to: 'toys#destroy'
