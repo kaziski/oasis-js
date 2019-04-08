9a7523230aeaf2c88cba3c5bf1d1174f314ed161
github username <github email address>
2019-04-07 22:41:07 -0400

    Chenge back code now it shows only actions selected.

--- app/models/action.rb
+++ app/models/action.rb
@@ -4,27 +4,36 @@ class Action < ApplicationRecord
   accepts_nested_attributes_for :plants_action
 
   def plants_action_attributes=(plant_action_attributes)
-      plant_action_attributes.values.each do |plant_action_attribute|
-      if plant_action_attribute[:plants_action][:month].present?
-        PlantsAction.create(
-          month: action_hash[:plants_action][:month],
-          action_id: action.id,
-          plant_id: self.id
-          )
-        end
+    plant_action_attributes.values.each do |plant_action_hash|
+    if plant_action_hash[:plants_action][:month].present?
+      PlantsAction.create(
+        month: action_hash[:plants_action][:month],
+        action_id: action.id,
+        plant_id: self.id
+        )
       end
+    end
   end
-
 end
 
+# name"=>"Pepper", 
+# "in_the_garden"=>"0", 
+# "edible"=>"1", 
+# "annual"=>"0", 
+# "note"=>"", 
+# "user_id"=>"4", "actions_attributes"=>{
+#   "0"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"1"}, 
+#   "1"=>{"action_name"=>"2", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-04"}}, "id"=>"2"}, 
+#   "2"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"3"}, 
+#   "3"=>{"action_name"=>"4", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-19"}}, "id"=>"4"}}} permitted: false>
 
-# "actions_attributes"=>{
+# "name"=>"Pepper", 
+# "in_the_garden"=>"0", 
+# "edible"=>"1", 
+# "annual"=>"0", 
+# "note"=>"", 
+# "user_id"=>"4", "actions_attributes"=>{
 #   "0"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"1"}, 
-#   "1"=>{"action_name"=>"2", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-05"}}, "id"=>"2"}, 
+#   "1"=>{"action_name"=>"2", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-12"}}, "id"=>"2"}, 
 #   "2"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"3"}, 
-#   "3"=>{"action_name"=>"4", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-25"}}, "id"=>"4"}}} permitted: false>, "commit"=>"Create Plant", "controller"=>"plants", "action"=>"create"}
-# "actions_attributes"=>{
-#   "0"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"1"}, 
-#   "1"=>{"action_name"=>"2", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-05"}}, "id"=>"2"}, 
-#   "2"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"3"}, 
-#   "3"=>{"action_name"=>"4", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-25"}}, "id"=>"4"}}} permitted: false>, "commit"=>"Create Plant", "controller"=>"plants", "action"=>"create"}
+#   "3"=>{"action_name"=>"4", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-30"}}, "id"=>"4"}}} permitted: false>,
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -4,7 +4,7 @@ class Plant < ApplicationRecord
   has_many :actions, through: :plants_action
   accepts_nested_attributes_for :actions, :allow_destroy => true
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true
   validates_inclusion_of :in_the_garden?, in: [true, false]
   validates_inclusion_of :edible?, in: [true, false]
   validates_inclusion_of :annual?, in: [true, false]
@@ -21,43 +21,22 @@ class Plant < ApplicationRecord
     where(user_id: user.id, annual: true )
   end
 
-  # def actions_attributes=(action_attributes)
-  #   action_attributes.values.each do |action_hash|
-      
-  #     if action_hash[:plants_action][:month].present?
-  #       action = Action.find_or_create_by(id: action_hash[:id])
-  #       binding.pry
-  #       PlantsAction.create(month: action_hash[:plants_action][:month], 
-  #         action_id: action.id, 
-  #         plant_id: self.id)
-  #       # pa_hash = plants_action_attributes[0]
-  #       self.actions << action
-  #     end
-  #   end
-  # end
-# "actions_attributes"=>{
-#   "0"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"1"}, 
-#   "1"=>{"action_name"=>"2", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-05"}}, "id"=>"2"}, 
-#   "2"=>{"action_name"=>"", "plants_action_attributes"=>{"0"=>{"month"=>""}}, "id"=>"3"}, 
-#   "3"=>{"action_name"=>"4", "plants_action_attributes"=>{"0"=>{"month"=>"2019-04-25"}}, "id"=>"4"}}} permitted: false>, "commit"=>"Create Plant", "controller"=>"plants", "action"=>"create"}
-
-  # def plants_action_attributes=(plant_action_attributes)
-  #   plant_action_attributes.values.each do |plant_action_attribute|
-  #     binding.pry
-  #     if plant_action_attribute[:month].present?
-  #       plant_action = PlantsAction.find_or_create_by(plant_action_attribute)
-  #       self.plants_action << plant_action
-  #     end
-  #   end
-  # end
-
 
   def actions_attributes=(action_attributes)
-    action_attributes.values.each do |action_hash|
-       if action_hash[:id].present?
-        action = Action.find_or_create_by(id: action_hash[:id])
+    action_attributes.values.each do |action_attribute|
+      if action_attribute[:action_name].present?
+        action = Action.find_or_create_by(id: action_attribute[:action_name])
         self.actions << action
       end
     end
   end
+
+  # def actions_attributes=(action_attributes)
+  #   action_attributes.values.each do |action_hash|
+  #      if action_hash[:id].present?
+  #       action = Action.find_or_create_by(id: action_hash[:id])
+  #       self.actions << action
+  #     end
+  #   end
+  # end
 end
