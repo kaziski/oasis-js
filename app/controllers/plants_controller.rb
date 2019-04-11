4bcdf5e852270d3bac09481a98ff9ca96de978b5
github username <github email address>
2019-04-11 01:58:01 -0400

    Create new plant and Edit a plant- add more action works. WIP: edit existing actions and dates.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -5,19 +5,12 @@ class PlantsController < ApplicationController
     @plants = Plant.all
   end
 
-  # def new
-  #   @plant = Plant.new
-  #   Action.all.each do |action|
-  #     @plant.plants_actions.build(action_id: action.id)
-  #   end
-  #   @plant.actions.build.plants_actions.build
-  # end
-
   def new
     @plant = Plant.new
     Action.all.each do |action|
       @plant.plants_actions.build(action_id: action.id)
     end
+    
   end
 
   def create
@@ -37,9 +30,7 @@ class PlantsController < ApplicationController
   end
 
   def update
-    binding.pry
-    @plant.update_attributes(plant_params
-    )
+    @plant.update(plant_params)
     redirect_to plant_path(@plant)
   end
 
@@ -63,14 +54,6 @@ class PlantsController < ApplicationController
 
   private 
 
-  # def plant_params
-  #   params.require(:plant).permit(:name, :in_the_garden, :edible, :annual, :user_id, :note,
-  #     :plants_actions_attributes => [:action_id, :month],
-  #     :actions_attributes => [:action_name, 
-  #       :plants_actions_attributes => [:month]]
-  #   )
-  # end
-
   def plant_params
     params.require(:plant).permit(:name, :in_the_garden, :edible, :annual, :user_id, :note,
       :plants_actions_attributes => [:action_id, :month],
@@ -79,20 +62,10 @@ class PlantsController < ApplicationController
     )
   end
 
-  def edit_params
-    params.require(:plant).permit(:name, :in_the_garden, :edible, :annual, :user_id, :note, 
-      :actions_attributes => [:action_name, :plants_action => [:month] ])
-  end
-
-# plant[:action][:action_name] 
-# plant[:action][:plants_action][:month]
   def set_plant
     @plant = Plant.find_by(id: params[:id])
   end
 end
-
-#this was the params in update b4 changing 
-# > <ActionController::Parameters {"utf8"=>"✓", "_method"=>"patch", 
-#   "authenticity_token"=>"WeSe9v7DiNh6K8Pqbe0o6uQSoeiasFmEvv+iLngdx9KAakd0nMmMDtHMvLe22exd0i07Fn4zPVPZvZakM5GmPQ==", 
-#   "plant"=>{"name"=>"Mint", "in_the_garden"=>"0", "edible"=>"1", "annual"=>"1", "note"=>"", 
-#     "action"=>{"action_name"=>"Minty action", "plants_action"=>{"month"=>"2019-04-27"}}}, 
+#Create is working. When editing, "Add more action" function works.
+#However, all existing actions need to be checked or it throws no action_name error.
+#if checked, though, creates dupes of actions and dates.
