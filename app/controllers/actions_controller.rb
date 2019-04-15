a1a3387695ca75a3afbd46d83e253418a45dc379
github username <github email address>
2019-04-15 15:07:48 -0400

    Clean up some code

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -2,12 +2,6 @@ class ActionsController < ApplicationController
 
   def new
     @plant = Plant.find_by(id: params[:plant_id]) 
-    # @plant = @plant.actions.build
-    # @action
-    # @plant = Plant.new
-    # @plant.actions.build.plants_actions.build
   end
 
-  def create
-  end   
 end
