9b62d9d04f3b8e3c57cb4f59205ad77236e71c40
github username <github email address>
2019-04-15 15:40:24 -0400

    Add view all actions and clean some more code

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -4,4 +4,12 @@ class ActionsController < ApplicationController
     @plant = Plant.find_by(id: params[:plant_id]) 
   end
 
+  def index
+    @actions = Action.all
+  end
+
+  def destroy
+    Action.find(params[:id]).destroy
+    redirect_to actions_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :plants do
     resources :actions
   end
-    
+  resources :actions, only: [:index, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
