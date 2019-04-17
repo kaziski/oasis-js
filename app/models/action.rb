b2a347427d50174f172184d15c18576764559f6a
github username <github email address>
2019-04-17 00:02:26 -0400

    Change a link in admin page and add validation to plants_action

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,7 +1,13 @@
 class Action < ApplicationRecord
   has_many :plants_actions
   has_many :plants, through: :plants_actions
-  accepts_nested_attributes_for :plants_actions, :allow_destroy => true
+  accepts_nested_attributes_for :plants_actions, :allow_destroy => true, :reject_if => :no_action_id
   validates :action_name, presence: true
  
+  private
+
+
+  def no_action_id(attributes)
+    attributes[:action_id] == nil
+  end
 end
--- app/models/plants_action.rb
+++ app/models/plants_action.rb
@@ -1,5 +1,13 @@
 class PlantsAction < ApplicationRecord
   belongs_to :plant, optional: true
   belongs_to :action, optional: true
-  # accepts_nested_attributes_for :actions, :allow_destroy => true
+  # validate :action_presence
+
+  # private
+
+  # def action_presence
+  #   if self.action == nil
+  #     errors.add(:action, "You need to check a checkbox first before selecting a date.")
+  #   end
+  # end
 end
