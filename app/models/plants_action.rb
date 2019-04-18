da224ee021c411c96acb0b95df631d86184720cc
github username <github email address>
2019-04-17 22:30:50 -0400

    Add custom message

--- app/models/plants_action.rb
+++ app/models/plants_action.rb
@@ -1,5 +1,7 @@
 class PlantsAction < ApplicationRecord
   belongs_to :plant, optional: true
   belongs_to :action, optional: true
-  validates_presence_of :action, :scope => :action_id
+  validates_presence_of :action, :scope => :action_id, :message => "must have a checkbox checked first, before selecting a date"
+
+
 end
