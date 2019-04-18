7b922bdf7b16ae40c74a830379cc3e595bf2780c
github username <github email address>
2019-04-18 13:42:27 -0400

    clean up code

--- app/models/plants_action.rb
+++ app/models/plants_action.rb
@@ -3,7 +3,6 @@ class PlantsAction < ApplicationRecord
   belongs_to :action, optional: true
   accepts_nested_attributes_for :action, :allow_destroy => true
   validates_presence_of :action, :scope => :action_id, :message => "must have a checkbox checked first, before selecting a date"
-  # validates_uniqueness_of :action, :scope => :action_id, :message => "Must be unique"
   validates_presence_of :action_date
 
   def action_attributes=(action_hash)
