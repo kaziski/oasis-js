9425dd4393f3940a3e4a77fc573d02806ac43fcd
github username <github email address>
2019-07-12 08:25:28 -0400

    Add ListenForClick() and modify button in erb

--- app/assets/javascripts/plant.js
+++ app/assets/javascripts/plant.js
@@ -1,9 +1,22 @@
-// $('/plants').addEventListener('DOMContentLoaded', function(event) {
-//   console.log('plant.js is loaded ...')
-//   handleOnSearchSubmit();
-// });
-
-$( document ).ready(function() {
+// $( document ).ready(function() {
+$(() => {
   console.log('plant.js is loaded ...')
-  // handleOnSearchSubmit();
+  listenForClick();
 });
+
+const listenForClick = () => {
+  $('button#show-button').on('click', () => {
+    event.preventDefault()
+    console.log('clicked!!!!!!!')
+    getPlants()
+  } )
+}
+
+const getPlants =  () => {
+  fetch(`/plants.json`)
+    .then((res) => res.json())
+    .then(data => {
+      $('button#show-button').hide()
+      $('#show-plants').html('oi!')
+    }) 
+}
